8bfbc9ccfc22405f4e2df6ba77aadf211a94fa84
F4Tornado <h.rovnyak@gmail.com>
2017-09-08 18:38:03 -0400

    sjm

--- js/sketch.js
+++ js/sketch.js
@@ -1,23 +1,17 @@
 var startScreen;
 var startButton;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   startScreen = loadImage("assets/AnimatedSupermarket.gif");
   startButton = loadImage("assets/startbutton.png")
 }
+
 function draw() {
-  image(startScreen,0,0,windowWidth,windowHeight);
-  image(startButton,windowWidth/2-100,windowHeight/2-50,100,50);
+  image(startScreen, 0, 0, windowWidth, windowHeight);
+  image(startButton, windowWidth / 2 - 100, windowHeight / 2 - 50, 100, 50);
 }
+
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-  image(startScreen,0,0,windowWidth,windowHeight);
-  image(startButton,windowWidth/2-100,windowHeight/2-50,100,50);
-}
-function MouseClicked() {
-if (mouseX == windowWidth/2-100 < windowWidth/2 && mouseY == windowHeight/2-50 < windowHeight/2)
-console.log("life works")
-else {
-  console.log("broken")
-}
 }
